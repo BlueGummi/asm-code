04eb58eb7653a61b8d131f04b24aee0c030abff0
gummi <thatstealthyowl@gmail.com>
2024-11-23 12:19:49 -0600

    fixed double-int

--- double-int/main.asm
+++ double-int/main.asm
@@ -3,10 +3,15 @@ section .bss
     result resb 10      ; we reserve space for the result string
 
 section .data
-    welcome db 'Enter a number, this will return the double of it.: '
+    welcome db 'Enter a number, this will return the double of it: '
     welcome_len equ $ - welcome ; welcome message
     returned db 'Double of your number is: '
     returned_len equ $ - returned ; length of thing to return
+    newline db 10
+    tempeax dd 0
+    tempebx dd 0
+    tempecx dd 0
+    tempedx dd 0
 section .text
     global _start
 
@@ -58,13 +63,29 @@ _start:
     inc ebx               ; increase digit count
     test eax, eax         ; check if EAX is zero
     jnz .convert           ; repeat if not zero
-
     ; write the result
-    mov edx, result + 10  ; point to the end of the result string, 10 gives newline
-    sub edx, ecx          ; calculate length of the result string
-    call print
-    int 0x80              ; call kernel
 
+     
+    mov edx, result      ; point to the end of the result string
+    sub edx, eax          ; calculate length of the result string
+    mov [tempedx], edx
+    xor edx, edx
+    mov [tempecx], ecx
+    xor ecx, ecx
+    mov [tempeax], eax
+    xor eax, eax
+    mov [tempebx], ebx
+    xor ebx, ebx
+    call return
+    mov edx, [tempedx]
+    mov ecx, [tempecx]
+    mov ebx, [tempebx]
+    mov eax, [tempeax]
+    call print
+
+    mov ecx, newline
+    mov edx, 1
+    call print            ; newline
     ; exit program
     mov eax, 1            ; sys_exit
     xor ebx, ebx          ; exit code 0
@@ -74,4 +95,10 @@ print:
     mov ebx, 1     ; file descriptor (stdout)
     mov eax, 4     ; system call number (sys_write)
     int 0x80       ; call kernel
+    ret
+
+return:
+    mov ecx, returned
+    mov edx, returned_len
+    call print
     ret
